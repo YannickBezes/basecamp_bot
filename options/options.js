7ad1afb36726d20bdb90aa1edccf1afe8bb69a0b
YannickBezes <yannick.bezes@gmail.com>
2021-09-19 10:55:46 +0200

    add list of authors and list of emoji by author

--- options/options.js
+++ options/options.js
@@ -1,4 +1,4 @@
-function drawRaw({ emoji, percentage }) {
+function drawRowEmoji({ emoji, percentage }) {
     const tableEmoji = document.getElementById('table-emoji');
 
     const tr = document.createElement('tr');
@@ -11,7 +11,7 @@ function drawRaw({ emoji, percentage }) {
 
 
     const tdPercentage = document.createElement('td');
-    tdPercentage.classList.add('td-percentage');
+    tdPercentage.classList.add('th-column-with-buttons');
     tr.appendChild(tdPercentage);
 
     // percentage
@@ -42,17 +42,155 @@ function drawRaw({ emoji, percentage }) {
     divButtonContainer.appendChild(buttonDelete);
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const listEmojis = await getEmojis();
+function drawRowAuthor(author) {
+    const tableAuthor = document.getElementById('table_author');
 
-    listEmojis.forEach(({ emoji, percentage }) => {
-        drawRaw({ emoji, percentage });
+    const tr = document.createElement('tr');
+    tableAuthor.appendChild(tr);
+
+
+    const td = document.createElement('td');
+    td.classList.add('th-column-with-buttons');
+    tr.appendChild(td);
+
+    // author
+    const span = document.createElement('span');
+    span.classList.add('author');
+    span.innerText = author;
+    td.appendChild(span);
+
+    // button container
+    const divButtonContainer = document.createElement('div');
+    divButtonContainer.classList.add('button-container');
+    td.appendChild(divButtonContainer);
+
+    // button edit
+    const buttonEdit = document.createElement('button');
+    buttonEdit.className = 'button edit';
+    buttonEdit.innerText = 'Modifier';
+    buttonEdit.addEventListener('click', () => editAuthor(author, span))
+    divButtonContainer.appendChild(buttonEdit);
+
+    // button delete
+    const buttonDelete = document.createElement('button');
+    buttonDelete.className = 'button delete';
+    buttonDelete.innerText = 'Supprimer';
+    buttonDelete.addEventListener('click', () => {
+        tr.remove()
     });
+    divButtonContainer.appendChild(buttonDelete);
 
+}
+
+function drawRowEmojiByAuthor({ emoji, author}) {
+    const tableEmojisByAuthor = document.getElementById('table_emoji-author');
+
+    const tr = document.createElement('tr');
+    tableEmojisByAuthor.appendChild(tr);
+
+    const tdEmoji = document.createElement('td');
+    tdEmoji.classList.add('emoji');
+    tdEmoji.innerText = emoji;
+    tr.appendChild(tdEmoji);
+
+
+    const tdPercentage = document.createElement('td');
+    tdPercentage.classList.add('th-column-with-buttons');
+    tr.appendChild(tdPercentage);
+
+    // author
+    const span = document.createElement('span');
+    span.classList.add('author');
+    span.innerText = author;
+    tdPercentage.appendChild(span);
+
+    // button container
+    const divButtonContainer = document.createElement('div');
+    divButtonContainer.classList.add('button-container');
+    tdPercentage.appendChild(divButtonContainer);
+
+    // button edit
+    const buttonEdit = document.createElement('button');
+    buttonEdit.className = 'button edit';
+    buttonEdit.innerText = 'Modifier';
+    buttonEdit.addEventListener('click', () => editEmojiByAuthor(emoji, span))
+    divButtonContainer.appendChild(buttonEdit);
+
+    // button delete
+    const buttonDelete = document.createElement('button');
+    buttonDelete.className = 'button delete';
+    buttonDelete.innerText = 'Supprimer';
+    buttonDelete.addEventListener('click', () => {
+        tr.remove()
+    });
+    divButtonContainer.appendChild(buttonDelete);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Table emojis
+    const listEmojis = await getEmojis();
+    listEmojis.forEach(({ emoji, percentage }) => {
+        drawRowEmoji({ emoji, percentage });
+    });
     document.getElementById('button-add').addEventListener('click', () => addEmoji());
 
-    document.getElementById('button-save').addEventListener('click', () => saveEmojis());
+
+    // Table authors
+    const listAuthors = await getFromStorage('listAuthors');
+    if (listAuthors !== null) {
+        listAuthors.forEach(author => drawRowAuthor(author));
+    }
+    document.getElementById('button-add_author').addEventListener('click', () => addAuthor());
+
+
+    // Table emojis by authors
+    const listEmojisByAuthor = await getFromStorage('listEmojisByAuthor');
+    if (listEmojisByAuthor !== null) {
+        listEmojisByAuthor.forEach(data => drawRowEmojiByAuthor(data));
+    }
+    document.getElementById('button-add_emoji-author').addEventListener('click', () => addEmojiByAuthor());
+
+
+    // Button save
+    document.getElementById('button-save').addEventListener('click', () => save());
 });
+
+function save() {
+    saveEmojis();
+    saveAuthors();
+    saveEmojisByAuthor();
+}
+
+function saveAuthors() {
+    const tableAuthor = document.getElementById('table_author');
+
+    const listAuthors = [];
+    for (let i = 0; i < tableAuthor.childElementCount; i++) {
+        const tr = tableAuthor.children[i];
+
+        const author = tr.getElementsByClassName('author')[0].innerText;
+
+        listAuthors.push(author);
+    }
+
+    chrome.storage.sync.set({ listAuthors });
+}
+
+function saveEmojisByAuthor() {
+    const tableEmojisByAuthor = document.getElementById('table_emoji-author');
+
+    const listEmojisByAuthor = [];
+    for (let i = 0; i < tableEmojisByAuthor.childElementCount; i++) {
+        const tr = tableEmojisByAuthor.children[i];
+
+        const emoji = tr.getElementsByClassName('emoji')[0].innerText;
+        const author = tr.getElementsByClassName('author')[0].innerText;
+
+        listEmojisByAuthor.push({ emoji, author });
+    }
+
+    chrome.storage.sync.set({ listEmojisByAuthor });
+}
 
 function saveEmojis() {
     const tableEmoji = document.getElementById('table-emoji');
@@ -82,6 +220,34 @@ function saveEmojis() {
     }
 }
 
+function addAuthor() {
+    const author = prompt('Auteur :');
+
+    if (author !== null && author !== '') {
+        drawRowAuthor(parseAuthor(author));
+    }
+}
+
+function addEmojiByAuthor() {
+    const emoji = prompt('Emoji :');
+
+    if (emoji !== null && emoji !== '') {
+        let author = prompt(`Auteur pour l'emoji ${emoji}`);
+
+        if (author !== null && author !== '') {
+            drawRowEmojiByAuthor({ emoji, author: parseAuthor(author) });
+        }
+    }
+}
+
+function parseAuthor(name) {
+    return removeDiacritics(name).toLowerCase()
+}
+
+function removeDiacritics(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
 function addEmoji() {
     const emoji = prompt('Emoji :');
 
@@ -92,7 +258,7 @@ function addEmoji() {
             if (parseFloat(percentage) >= 1 && percentage !== '1.00') {
                 percentage /= 100;
             }
-            drawRaw({ emoji, percentage });
+            drawRowEmoji({ emoji, percentage });
         }
     }
 }
@@ -108,6 +274,22 @@ function editEmoji(emoji, span) {
     }
 }
 
+function editEmojiByAuthor(emoji, span) {
+    let author = prompt(`Editer auteur pour l'emoji ${emoji}`, span.innerText);
+
+    if (author !== null && author !== '') {
+        span.innerText = author;
+    }
+}
+
+function editAuthor(author, span) {
+    let newAuthor = prompt('Editer l\' auteur', author);
+
+    if (newAuthor !== null && newAuthor !== '') {
+        span.innerText = parseAuthor(newAuthor);
+    }
+}
+
 async function getEmojis() {
     return new Promise(resolve => {
         chrome.storage.sync.get('listEmojis', ({ listEmojis }) => {
@@ -120,5 +302,19 @@ async function getEmojis() {
                 ]);
             }
         });
+    });
+}
+
+async function getFromStorage(key) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get(key, resolve);
+    }).then(result => {
+        if (key === null) {
+            return result;
+        } else if (typeof  result[key] === 'undefined') {
+            return  null;
+        } else {
+            return result[key];
+        }
     });
 }
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const QUERY_SELECTOR_AUTHOR = '.thread-entry__author'
+
 async function getFromStorage(key) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         chrome.storage.sync.get(key, resolve);
     }).then(result => {
         if (key == null) return result;
@@ -8,38 +10,48 @@ async function getFromStorage(key) {
 }
 
 async function putBoosts() {
+    // Get name of user
     let name = removeDiacritics(await getFromStorage("name")).toLowerCase();
 
+    // Get list of articles
     const articles = await findPostWithoutBoosts(name);
 
     if (articles.length > 0) {
+        // Ask to user if he want to boosts the posts
         const res = confirm(`Do you want to boosts ${articles.length} posts ?`);
 
         if (res) {
-            articles.forEach(async ar => {
+            for (const ar of articles) {
                 const boostDiv = ar.querySelector(':scope label[aria-label="Add a boost"]');
 
                 boostDiv.click();
                 await sleep(200);
                 const input = boostDiv.querySelector('input');
-                input.value = await getEmoji();
+
+                // Get emoji for author of the boosts
+                input.value = await getEmoji(parseAuthorName(ar.querySelector(QUERY_SELECTOR_AUTHOR)));
 
                 await sleep(200);
                 ar.querySelector('input[type="submit"]').click() // Send boost
                 await sleep(300);
-            });
+            }
+
+            // Scroll to the last articles
+            const lastArticle = articles[articles.length - 1];
+            scrollTo(0, lastArticle.offsetTop + lastArticle.offsetHeight);
         }
-        // Scroll to the last articles
-        const lastArticle = articles[articles.length - 1];
-        scrollTo(0, lastArticle.offsetTop + lastArticle.offsetHeight);
     } else {
         alert('No articles has been found');
     }
 }
 
-async function getEmoji() {
-    const emojis = await getEmojis();
+async function getEmoji(author) {
+    const emojisOverrideByAuthor = await getFromStorage('listEmojisByAuthor');
 
+    if (emojisOverrideByAuthor.keys().includes(author)) {
+        return emojisOverrideByAuthor[author];
+    }
+    const emojis = await getEmojis();
     return getRandom(emojis);
 }
 
@@ -60,13 +72,17 @@ function getRandom(emojis) {
     return results[lastIndex];
 }
 
-async function findPostWithoutBoosts(name, max_date) {
+async function findPostWithoutBoosts(name) {
     let findAnArticleWidthABoost = false;
+    let maxDateHasPassed = false;
     let offset = 0;
 
     let articlesWithoutBoosts = [];
+    // const maxDate = getFromStorage('maxDate');
 
     let lastLengthArticles = 0;
+    // Stop to find articles when we have found an articles
+    // while (!findAnArticleWidthABoost && maxDateHasPassed) {
     while (!findAnArticleWidthABoost) {
         let articles = Array.from(document.querySelectorAll(".thread-entry.thread-entry--with-discuss"));
 
@@ -79,20 +95,21 @@ async function findPostWithoutBoosts(name, max_date) {
                 console.log('stop because there is no articles');
             }
 
-            articles.forEach(ar => {
-                if (findAnArticleWidthABoost) return; // Stop the loop
-                const author = parseAuthorName(ar.querySelector('.thread-entry__author'));
+            for (const ar of articles) {
+                const author = parseAuthorName(ar.querySelector(QUERY_SELECTOR_AUTHOR));
+                const listOfAuthors = await getFromStorage('listAuthors');
 
-                if (author !== name) { // If it's not me boosts message
+                if (author !== name && (listOfAuthors.includes(author) || listOfAuthors.length === 0)) {
                     // Check if we already have boosts this post
                     if (!alreadyBoosts(ar, name)) {
                         articlesWithoutBoosts.push(ar);
                     } else {
                         findAnArticleWidthABoost = true;
+                        return; // Stop the loop
                     }
                 }
                 offset++; // Increment offset
-            });
+            }
         }
         scrollBy(0, window.innerHeight);
         await sleep(500);
